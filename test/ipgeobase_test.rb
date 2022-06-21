b72833311bd8c7c75772ace3e5b3509021bc0565
alex <alextar994@gmail.com>
2022-06-21 15:17:23 +0300

    Bugfix теста

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class IpgeobaseTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -9,23 +9,23 @@ class IpgeobaseTest < Minitest::Test
 
   def test_lookup
     response   = "<query>\n"\
-    "  <status>success</status>\n"\
-    "  <country>United States</country>\n"\
-    "  <countryCode>US</countryCode>\n"\
-    "  <region>VA</region>\n"\
-    "  <regionName>Virginia</regionName>\n"\
-    "  <city>Ashburn</city>\n"\
-    "  <zip>20149</zip>\n"\
-    "  <lat>39.03</lat>\n"\
-    "  <lon>-77.5</lon>\n"\
-    "  <timezone>America/New_York</timezone>\n"\
-    "  <isp>Google LLC</isp>\n"\
-    "  <org>Google Public DNS</org>\n"\
-    "  <as>AS15169 Google LLC</as>\n"\
-    "  <query>8.8.8.8</query>\n"\
-    "</query>"
-    stub_request(:any, "http://ip-api.com").to_return(body: response, status: 200)
-    
+                 "  <status>success</status>\n"\
+                 "  <country>United States</country>\n"\
+                 "  <countryCode>US</countryCode>\n"\
+                 "  <region>VA</region>\n"\
+                 "  <regionName>Virginia</regionName>\n"\
+                 "  <city>Ashburn</city>\n"\
+                 "  <zip>20149</zip>\n"\
+                 "  <lat>39.03</lat>\n"\
+                 "  <lon>-77.5</lon>\n"\
+                 "  <timezone>America/New_York</timezone>\n"\
+                 "  <isp>Google LLC</isp>\n"\
+                 "  <org>Google Public DNS</org>\n"\
+                 "  <as>AS15169 Google LLC</as>\n"\
+                 "  <query>8.8.8.8</query>\n"\
+                 '</query>'
+    stub_request(:get, 'http://ip-api.com/xml/8.8.8.8').to_return(body: response, status: 200)
+
     ip_address = '8.8.8.8'
 
     expected_city        = 'Ashburn'
@@ -34,7 +34,7 @@ class IpgeobaseTest < Minitest::Test
     expected_lat         = '39.03'
     expected_lon         = '-77.5'
 
-    result = Ipgeobase.lookup('8.8.8.8')
+    result = Ipgeobase.lookup(ip_address)
 
     assert { result.city        == expected_city }
     assert { result.country     == expected_country }
