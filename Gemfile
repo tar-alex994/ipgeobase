e207c366bacdb72c3afb5d1398c008b7dbaf3f60
alex <alextar994@gmail.com>
2022-06-21 15:16:21 +0300

    Реализуй функцию lookup

--- Gemfile
+++ Gemfile
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ipgeobase.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem 'rake', '~> 13.0'
 
-gem "minitest", "~> 5.0"
-gem "minitest-power_assert"
-gem "webmock"
+gem 'minitest", "~> 5.0'
+gem 'minitest-power_assert'
+gem 'webmock'
+
+gem 'addressable', '~> 2.7'
+gem 'nokogiri-happymapper'
--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,41 @@
 # frozen_string_literal: true
 
-require_relative "ipgeobase/version"
+require_relative 'ipgeobase/version'
+require 'happymapper'
+require 'net/http'
+require 'addressable'
 
 module Ipgeobase
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.lookup(ip_address)
+    # сформировать адресс для запроса на ip-api
+    uri = Addressable::Template.new('http://ip-api.com/xml/{segment}')
+                               .expand({ 'segment' => ip_address })
+
+    # получить данные от ip-api с помощью запроса
+    response = Net::HTTP.get(uri)
+
+    # вытащить метаданные из ответа
+    ip_metadata = IpMetadata.parse_from_xml(response)
+
+    # возврат объекта с метаданными
+    IpMetadata.new(ip_metadata)
+  end
+
+  class IpMetadata
+    attr_reader :city, :country, :country_code, :lat, :lon
+
+    def initialize(ip_metadata)
+      @country_code = ip_metadata[:country_code]
+      @country      = ip_metadata[:country]
+      @city         = ip_metadata[:city]
+      @lat          = ip_metadata[:lat]
+      @lon          = ip_metadata[:lon]
+    end
+
+    def self.parse_from_xml(xml)
+      ip_metadata = HappyMapper.parse(xml)
+      { city: ip_metadata.city, country: ip_metadata.country, lat: ip_metadata.lat,
+        country_code: ip_metadata.country_code, lon: ip_metadata.lon }
+    end
+  end
 end
